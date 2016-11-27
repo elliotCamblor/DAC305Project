b824c5d0e384b9003949c19517a0c26dae8f50f8
Elliot Campbell <elliotgithub@gmail.com>
2016-11-27 18:46:34 -0500

    can now switch to blue by holding Q

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	private Rigidbody2D myRigidbody;
+	private SpriteRenderer mySprite;
+	private CircleCollider2D myCollider;
+	public PhysicsMaterial2D blueMat;
 
 	[SerializeField]
-	 private float movementspeed;
+	private float movementspeed;
 
 
 	// Use this for initialization
 	void Start () {
-	
 		myRigidbody = GetComponent<Rigidbody2D> ();
+		mySprite = GetComponent<SpriteRenderer> ();
+		myCollider = GetComponent<CircleCollider2D> ();
 	}
 	
 	// Update is called once per frame
@@ -21,10 +25,30 @@ public class PlayerController : MonoBehaviour
 		float horizontal = Input.GetAxis ("Horizontal");
 
 		HandleMovement (horizontal);
+		if (Input.GetKey(KeyCode.Q)){
+			SetBlue();
+		} else {
+			SetNeutral ();
+		}
 	}
 
 	private void HandleMovement(float horizontal)
 	{
 		myRigidbody.velocity = new Vector2 (horizontal * movementspeed, myRigidbody.velocity.y);
+	}
+
+	private void SetBlue() {
+
+		mySprite.color = Color.blue;
+		myCollider.sharedMaterial = blueMat;
+		myCollider.enabled = false;
+		myCollider.enabled = true;
+	}
+
+	private void SetNeutral() {
+		mySprite.color = Color.white;
+		myCollider.sharedMaterial = null;
+		myCollider.enabled = false;
+		myCollider.enabled = true;
 	}
 }
